c2ef91abb32ac6cc693e5d7700fa70a09beb8485
GeneTinderholm <tinderholmgene@gmail.com>
2017-11-20 16:17:53 -0600

    everything actually works

--- routes/index.js
+++ routes/index.js
@@ -28,27 +28,73 @@ router.get('/', function(req, res, next) {
 	let fillLikes = () => {
 		if (insanity <= 5) {
 			likes.push('You like mild colors.');
+			dislikes.push('You dislike dark and scary colors.');
 		} else {
 			likes.push('Your like dark and scary colors');
+			dislikes.push('You dislike mild colors.');
 		}
 		if (goodEvil === 'good') {
 			likes.push('You like being humble and kind');
+			dislikes.push('You dislike being bossy.');
 		} else {
 			likes.push('You like being bossy');
+			dislikes.push('You dislike being humble and kind.');
 		}
 		if (select_colors) {
 			likes.push(`Your favorite color is ${select_colors}`);
+			dislikes.push('You dislike invisible gremlins.');
 		}
 		if (favFood) {
-			likes.push(`Your favorite color is ${favFood}`);
+			likes.push(`Your favorite food is ${favFood}`);
+			dislikes.push('You dislike lutefisk!');
 		}
 	};
 	fillLikes();
+	let insanityMeter;
+	if(insanity < 5){
+		insanityMeter = "super insane";
+	} else {
+		insanityMeter = "super duper insane";
+	}
+	let biography;
+	if(goodEvil && favFood && select_colors){
+	biography = `You were born to a ${goodEvil} family. You went to school, where you learned to love ${favFood} and ${select_colors}. Later you attended Viking Code School where you went ${insanityMeter}!`;
+} else {
+	biography = "You need to fill out the form before we can tell you your life story.";
+}
+
+	let resume;
+	switch(select_colors){
+		case 'red':
+			resume = "You can't seem to avoid fast cars.";
+			break;
+		case 'blue':
+			resume = "You are infatuated with the sky.";
+			break;
+		case 'green':
+			resume = "You gain great delight from being outdoors.";
+			break;
+		case 'yellow':
+			resume = "You are a morning person."
+			break;
+	}
+
+	if(goodEvil === 'good'){
+		resume += " You had a job taking care of rescued orphan puppies.";
+	}else {
+		resume += " You had a job breaking into people's homes.";
+	}
+	if(!select_colors || !goodEvil){
+		resume = "You need to fill out the form before we can give you your resume.";
+	}
 	res.render('index', {
 		goodEvil: goodEvil,
 		select_colors: select_colors,
 		insane_friends: insane_friends,
-		likes: likes
+		likes: likes,
+		dislikes: dislikes,
+		biography: biography,
+		resume: resume
 	});
 	console.log(req.cookies.favorites || 'No favorites');
 });
